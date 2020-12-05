e18d59bf174ec8bea03708158b1ec1696d1d6afe
nbkobayashi <73633610+nbkobayashi@users.noreply.github.com>
2020-12-05 11:41:54 +0900

    Add navigations

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
+import { StackNavigationProp } from "@react-navigation/stack";
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "Home">;
+};
+
+export default function HomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="to AddScreen" onPress={() => navigation.navigate("Add")} />
       <StatusBar style="auto" />
     </View>
   );
