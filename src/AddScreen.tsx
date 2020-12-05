f8b2bf154fbbfa01d264b82e38c12881a872e870
nbkobayashi <73633610+nbkobayashi@users.noreply.github.com>
2020-12-05 13:41:08 +0900

    Add camera and preview

--- src/AddScreen.tsx
+++ src/AddScreen.tsx
@@ -1,19 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-
+import React, { useState, useRef } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "Add">;
 };
+const screenWidth = Dimensions.get("screen").width;
 
 export default function AddScreen({ navigation }: Props) {
+  const [titleText, setTitleText] = useState("");
+  const [pictureURI, setPictureURI] = useState("");
+  const pictureURICache = useRef("");
+
+  // カメラを起動して画像を取得する
+  const takePictureFromCameraAsync = async () => {
+    const result = await ImagePicker.launchCameraAsync({
+      aspect: [3, 4],
+      allowsEditing: true
+    });
+    if (result.cancelled) return;
+    setPictureURI(result.uri);
+    // useRefにも保存する
+    pictureURICache.current = result.uri;
+  };
+
+  React.useEffect(() => {
+    return () => {
+      // キャッシュを削除
+      if (pictureURICache.current !== "") {
+        FileSystem.deleteAsync(pictureURICache.current, { idempotent: true });
+      }
+    };
+  }, []);
+
+  const Preview = () => {
+    return <Image style={styles.preview} source={{ uri: pictureURI }} />;
+  };
+
+  const Camera = () => {
+    return (
+      <TouchableOpacity
+        style={styles.cameraButton}
+        onPress={takePictureFromCameraAsync}
+      >
+        <Icon name="camera" size={100} />
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Button title="to HomeScreen" onPress={() => navigation.goBack()} />
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          style={styles.titleInputConatiner}
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            style={styles.titleInput}
+            placeholder="タイトル"
+            onChangeText={value => setTitleText(value)}
+            maxLength={100}
+          />
+        </KeyboardAvoidingView>
+        <View style={styles.previewContainer}>
+          {pictureURI ? <Preview /> : <Camera />}
+        </View>
+        <Button title="to HomeScreen" onPress={() => navigation.goBack()} />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -23,5 +93,36 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  titleInputConatiner: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  titleInput: {
+    flex: 0.9,
+    color: "#000",
+    fontSize: 20,
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    padding: 3
+  },
+  cameraButton: {
+    width: screenWidth * 0.8,
+    height: (screenWidth * 0.8 * 4) / 3,
+    borderRadius: 30,
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  previewContainer: {
+    flex: 7,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  preview: {
+    width: screenWidth * 0.8,
+    height: (screenWidth * 0.8 * 4) / 3
   }
 });
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,17 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 import { StackNavigationProp } from "@react-navigation/stack";
+import * as ImagePicker from "expo-image-picker";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "Home">;
 };
 
 export default function HomeScreen({ navigation }: Props) {
+  const [hasPermission, setHasPermission] = useState(false);
+  // アプリの初期化
+  const initAppAsync = async () => {
+    // カメラのアクセス権限を取得
+    const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
+    const cameraRollPermission = await ImagePicker.requestCameraRollPermissionsAsync();
+    const granted = cameraPermission.granted && cameraRollPermission.granted;
+    setHasPermission(granted);
+  };
+
+  // 初期化処理
+  React.useEffect(() => {
+    initAppAsync();
+  }, []);
+
+  const UnPermission = () => {
+    return <Text>カメラ及びカメラロールへのアクセス許可が有りません。</Text>;
+  };
+
   return (
     <View style={styles.container}>
-      <Button title="to AddScreen" onPress={() => navigation.navigate("Add")} />
+      {/* 権限が無かった時 */}
+      {!hasPermission && <UnPermission />}
+      {/* 権限が有った時 */}
+      {hasPermission && (
+        <Button
+          title="to AddScreen"
+          onPress={() => navigation.navigate("Add")}
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
